92a8807ed6993631b06e8bf3c7389a3a17d1b4cb
Lucas Rocha <122882995+lucas1791@users.noreply.github.com>
2024-01-13 10:26:23 -0300

    traduzido

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+# Import functions from the crypto and decrypto modules.
 from decrypto import decrypt
 from crypto import encrypt
 
-print('\033[32mBem vindo ao programa de criptografia, use esse programa para salvar mensagens importantes a fim de descrição eficaz.\033[m')
+# Greeting message.
+print('\033[32mWelcome to the encryption program. Use this program to securely store important messages for effective description.\033[m')
 
 print('\033[36m=\033[m'*119)
 
 print('\033[32mOPTIONS:\033[m')
 
+# Menu logic
 while True:
     try:
-        options = int(input('\n\n\033[34mDigite [1] para Criptografar um texto ou [2] para Descriptografar. Ou digite [3] para sair: \033[m'))
+        options = int(input('\n\n\033[34mEnter [1] to Encrypt a text or [2] to Decrypt. Or enter [3] to exit: \033[m'))  # Provides options for the user to choose between encryption, decryption, and exit. Ensures that the user does not input incorrect characters.
 
         if options == 1:
             print()
@@ -19,8 +22,7 @@ while True:
         elif options == 3:
             break
         elif options != 1 or options != 2 or options != 3:
-            print('\033[31mERRO, ESSA OPÇÃO NÃO EXISTE!\033[m')
+            print('\033[31mERROR, THIS OPTION DOES NOT EXIST!\033[m')
         
     except ValueError:
-        print('\033[31mERRO, ESSA OPÇÃO NÃO EXISTE!\033[m')
-
+        print('\033[31mERROR, THIS OPTION DOES NOT EXIST!\033[m')
